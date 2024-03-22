db96a544e54754eb194604351a4923595ef49516
xyguo <xinyuan168@dingtalk.com>
2024-03-22 18:34:09 +0800

    first

---
+++ datas/Config.js
@@ -0,0 +1,12 @@
+const LEVEL = {
+    normal: 'normal',  // Adjust the value as necessary
+    warning: 'warning',  // Adjust the value as necessary
+    danger: 'danger'  // Adjust the value as necessary
+};
+
+const Config = {
+    ITEMS_SAVE_KEY: 'your_items_key_here', // Adjust the key as necessary
+    LEVEL: LEVEL
+};
+
+module.exports = Config;
